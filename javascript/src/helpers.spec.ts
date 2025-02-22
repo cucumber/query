ab259278ee855d7800c7e3b0496042488df0c073
David Goss <david@davidgoss.co>
2025-02-22 17:52:58 +0000

    Upgrade ESLint, rework config, split out Prettier (#72)

--- javascript/src/helpers.spec.ts
+++ javascript/src/helpers.spec.ts
@@ -1,6 +1,8 @@
 import assert from 'node:assert'
-import { comparatorByStatus } from './helpers'
+
 import { TestStepResult, TestStepResultStatus } from '@cucumber/messages'
+
+import { comparatorByStatus } from './helpers'
 
 describe('comparatorByStatus', () => {
   function resultWithStatus(status: TestStepResultStatus) {
@@ -8,8 +10,26 @@ describe('comparatorByStatus', () => {
   }
 
   it('puts the more severe status after the less severe', () => {
-    assert.strictEqual(comparatorByStatus(resultWithStatus(TestStepResultStatus.PASSED), resultWithStatus(TestStepResultStatus.FAILED)), -1)
-    assert.strictEqual(comparatorByStatus(resultWithStatus(TestStepResultStatus.FAILED), resultWithStatus(TestStepResultStatus.PASSED)), 1)
-    assert.strictEqual(comparatorByStatus(resultWithStatus(TestStepResultStatus.FAILED), resultWithStatus(TestStepResultStatus.FAILED)), 0)
+    assert.strictEqual(
+      comparatorByStatus(
+        resultWithStatus(TestStepResultStatus.PASSED),
+        resultWithStatus(TestStepResultStatus.FAILED)
+      ),
+      -1
+    )
+    assert.strictEqual(
+      comparatorByStatus(
+        resultWithStatus(TestStepResultStatus.FAILED),
+        resultWithStatus(TestStepResultStatus.PASSED)
+      ),
+      1
+    )
+    assert.strictEqual(
+      comparatorByStatus(
+        resultWithStatus(TestStepResultStatus.FAILED),
+        resultWithStatus(TestStepResultStatus.FAILED)
+      ),
+      0
+    )
   })
 })
