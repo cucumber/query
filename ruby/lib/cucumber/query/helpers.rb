6559a25c179366857edee7658223be5f12c1ca1e
Luke Hill <lukehill_uk@hotmail.com>
2025-09-05 19:44:34 +0100

    Ported over helpers from JS

---
+++ ruby/lib/cucumber/query/helpers.rb
@@ -0,0 +1,23 @@
+require 'cucumber/messages/test_step_result_status'
+
+module Cucumber
+  module Query
+    module Helpers
+      def status_ordinal(status)
+        [
+          TestStepResultStatus::UNKNOWN,
+          TestStepResultStatus::PASSED,
+          TestStepResultStatus::SKIPPED,
+          TestStepResultStatus::PENDING,
+          TestStepResultStatus::UNDEFINED,
+          TestStepResultStatus::AMBIGUOUS,
+          TestStepResultStatus::FAILED
+        ].index(status)
+      end
+
+      def assert(target, failure_message)
+        raise StandardError, failure_message unless target
+      end
+    end
+  end
+end
