3e7247051d61d2774324045e85f8684954f06e08
Luke Hill <lukehill_uk@hotmail.com>
2025-11-28 18:00:19 +0000

    Remove legacy tests

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -3,10 +3,6 @@
 require 'cucumber'
 require 'cucumber/core'
 
-require_relative 'support/runner_helper'
-require_relative 'support/gherkin_helper'
-
 RSpec.configure do |c|
-  c.include RunnerHelper
-  c.extend GherkinHelper
+  :no_op
 end
