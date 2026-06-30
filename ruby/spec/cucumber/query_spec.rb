84e15603717bd2e14fd604457cced4629525b2f6
Luke Hill <lukehill_uk@hotmail.com>
2026-06-30 18:28:20 +0100

    Tidy up test iteration code to ensure we only select files that will run (let alone pass!)

--- ruby/spec/cucumber/query_spec.rb
+++ ruby/spec/cucumber/query_spec.rb
@@ -3,36 +3,6 @@
 require 'json'
 require 'cucumber/query'
 
-source_names = %w[attachments empty hooks minimal rules].freeze
-
-queries = {
-  'findAllPickles' => ->(query) { query.find_all_pickles.length },
-  'findAllTestCases' => ->(query) { query.find_all_test_cases.length },
-  'findAllTestSteps' => ->(query) { query.find_all_test_steps.length },
-  'findTestCaseBy' => lambda do |query|
-    results = {}
-    results['testCaseStarted'] = query.find_all_test_case_started.map { |message| query.find_test_case_by(message).id }
-    results['testCaseFinished'] = query.find_all_test_case_finished.map { |message| query.find_test_case_by(message).id }
-    results['testStepStarted'] = query.find_all_test_step_started.map { |message| query.find_test_case_by(message).id }
-    results['testStepFinished'] = query.find_all_test_step_finished.map { |message| query.find_test_case_by(message).id }
-    results
-  end,
-  'findTestStepBy' => lambda do |query|
-    results = {}
-    results['testStepStarted'] = query.find_all_test_step_started.map { |message| query.find_test_step_by(message).id }
-    results['testStepFinished'] = query.find_all_test_step_finished.map { |message| query.find_test_step_by(message).id }
-    results
-  end,
-  'findPickleBy' => lambda do |query|
-    results = {}
-    results['testCaseStarted'] = query.find_all_test_case_started.map { |message| query.find_pickle_by(message).name }
-    results['testCaseFinished'] = query.find_all_test_case_finished.map { |message| query.find_pickle_by(message).name }
-    results['testStepStarted'] = query.find_all_test_step_started.map { |message| query.find_pickle_by(message).name }
-    results['testStepFinished'] = query.find_all_test_step_finished.map { |message| query.find_pickle_by(message).name }
-    results
-  end
-}.freeze
-
 RSpec.describe Cucumber::Query do
   def testdata_path(name)
     File.expand_path("../../../testdata/src/#{name}", __dir__)
@@ -42,11 +12,53 @@ RSpec.describe Cucumber::Query do
     Cucumber::Messages::Helpers::NdjsonToMessageEnumerator.new(File.read(path))
   end
 
+  queries = {
+    'findAllPickles' => ->(query) { query.find_all_pickles.length },
+    'findAllTestCases' => ->(query) { query.find_all_test_cases.length },
+    'findAllTestSteps' => ->(query) { query.find_all_test_steps.length },
+    'findTestCaseBy' => lambda do |query|
+      results = {}
+      results['testCaseStarted'] = query.find_all_test_case_started.map { |message| query.find_test_case_by(message).id }
+      results['testCaseFinished'] = query.find_all_test_case_finished.map { |message| query.find_test_case_by(message).id }
+      results['testStepStarted'] = query.find_all_test_step_started.map { |message| query.find_test_case_by(message).id }
+      results['testStepFinished'] = query.find_all_test_step_finished.map { |message| query.find_test_case_by(message).id }
+      results
+    end,
+    'findTestStepBy' => lambda do |query|
+      results = {}
+      results['testStepStarted'] = query.find_all_test_step_started.map { |message| query.find_test_step_by(message).id }
+      results['testStepFinished'] = query.find_all_test_step_finished.map { |message| query.find_test_step_by(message).id }
+      results
+    end,
+    'findPickleBy' => lambda do |query|
+      results = {}
+      results['testCaseStarted'] = query.find_all_test_case_started.map { |message| query.find_pickle_by(message).name }
+      results['testCaseFinished'] = query.find_all_test_case_finished.map { |message| query.find_pickle_by(message).name }
+      results['testStepStarted'] = query.find_all_test_step_started.map { |message| query.find_pickle_by(message).name }
+      results['testStepFinished'] = query.find_all_test_step_finished.map { |message| query.find_pickle_by(message).name }
+      results
+    end
+  }.freeze
+
+  # This list needs to be tightly controlled.
+  #   New items should be added when new queries have been adapted to work with them
+  #     AND
+  #   When the requisite sample data result files have been created
+  enabled_files = %w[attachments empty hooks minimal rules].freeze
+
+  files_in_testdata = Dir.glob(File.expand_path("../../../testdata/src/*.ndjson", __dir__))
+  files_in_testdata = files_in_testdata.select do |file|
+    enabled_files.any? do |enabled_file|
+      file.match?("/#{enabled_file}.ndjson")
+    end
+  end
+  filenames_in_testdata = files_in_testdata.map { it.split('/').last.split('.').first }
+
   subject(:query) { described_class.new(repository) }
 
   let(:repository) { Cucumber::Repository.new }
 
-  source_names.product(queries.to_a).each do |source_name, (query_name, query_proc)|
+  filenames_in_testdata.product(queries.to_a).each do |source_name, (query_name, query_proc)|
     describe "Query: '#{query_name}'. testdata source: '#{source_name}'" do
       let(:source) { testdata_path("#{source_name}.ndjson") }
       let(:expected_results_file) { testdata_path("#{source_name}.#{query_name}.results.json") }
